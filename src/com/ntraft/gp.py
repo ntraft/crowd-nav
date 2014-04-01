af2ee07f173bd4515f854e25cfe2d70310e8d54c
Neil Traft <ntraft@gmail.com>
2014-04-01 13:54:54 -0700

    Created a new GaussianProcess class and demo. Still tweaking it.

---
+++ src/com/ntraft/gp.py
@@ -0,0 +1,85 @@
+'''
+Created on Apr 1, 2014
+
+@author: ntraft
+'''
+from __future__ import division
+import numpy as np
+
+OBS_NOISE = 0.00005	# noise variance
+
+def sq_exp(a, b):
+	""" GP squared exponential kernel """
+	kernelParameter = 1 # l^2 in the formulas
+	sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
+	return np.exp(-.5 * (1/kernelParameter) * sqdist)
+
+class GaussianProcess:
+	'''
+	Represents a Gaussian process that can be sampled from. Samples are taken
+	at each test point, given the supplied observations.
+	'''
+
+	def __init__(self, observations, timepoints, kernel=sq_exp):
+		'''
+		Creates a new Gaussian process from the given observations.
+		'''
+		zt = observations[:,0]
+		zx = observations[:,1]
+		zy = observations[:,2]
+		self.timepoints = timepoints
+		
+		# covariance of observations
+		K = kernel(zt, zt)
+		L = np.linalg.cholesky(K + OBS_NOISE*np.eye(len(zt)))
+		
+		# compute the mean at our test points
+		Lk = np.linalg.solve(L, kernel(zt, timepoints))
+		self.xmu = np.dot(Lk.T, np.linalg.solve(L, zx))
+		self.ymu = np.dot(Lk.T, np.linalg.solve(L, zy))
+		
+		# compute the variance at our test points
+		K_ = kernel(timepoints, timepoints)
+		self.L = np.linalg.cholesky(K_ + 1e-6*np.eye(K_.shape[0]) - np.dot(Lk.T, Lk))
+	
+	def sample(self, n=1):
+		'''
+		Draw n samples from the gaussian process.
+		'''
+		sz = (len(self.timepoints), n)
+		x_post = self.xmu.reshape(-1,1) + np.dot(self.L, np.random.normal(size=sz))
+		y_post = self.ymu.reshape(-1,1) + np.dot(self.L, np.random.normal(size=sz))
+		return np.hstack((self.timepoints, x_post, y_post))
+
+
+if __name__ == "__main__":
+	import matplotlib
+	# The 'MacOSX' backend appears to have some issues on Mavericks.
+	import sys
+	if sys.platform.startswith('darwin'):
+		matplotlib.use('TkAgg')
+	import matplotlib.pyplot as pl
+	
+	# This is the true unknown function we are trying to approximate
+	x1 = lambda x: x.flatten()
+	x2 = lambda x: np.sin(0.9*x).flatten()
+	
+	# Sample some input points and noisy versions of the function evaluated at
+	# these points.
+	N = 10		# number of training points
+	n = 500		# number of test points
+	T = np.random.uniform(-5, 5, size=(N,1))
+	x = x1(T) + OBS_NOISE*np.random.randn(N)
+	y = x2(T) + OBS_NOISE*np.random.randn(N)
+	z = np.hstack((T, x, y))
+	
+	# points we're going to make predictions at.
+	Ttest = np.linspace(-5, 5, n).reshape(-1,1)
+	
+	# draw 10 samples from the posterior
+	gp = GaussianProcess(z, Ttest)
+	samples = gp.sample(10)
+	pl.clf()
+	pl.plot(samples[1], samples[2])
+	pl.title('Ten samples from the GP posterior')
+	pl.axis([-5, 5, -3, 3])
