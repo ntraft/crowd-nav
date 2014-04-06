c2d20aaba84ec049dafee972b0473503fea9e9a3
Neil Traft <ntraft@gmail.com>
2014-04-05 23:11:40 -0700

    Playing with hyperparameters and goal points really clears up some issues.

--- src/com/ntraft/gpdemo.py
+++ src/com/ntraft/gpdemo.py
@@ -2,7 +2,6 @@ from __future__ import division
 import numpy as np
 import com.ntraft.covariance as cov
 from com.ntraft.gp import GaussianProcess
-from util import OBS_NOISE
 import matplotlib
 # The 'MacOSX' backend appears to have some issues on Mavericks.
 import sys
@@ -13,23 +12,26 @@ import matplotlib.pyplot as pl
 # This is the true unknown function we are trying to approximate
 x1 = lambda x: x.flatten()
 x2 = lambda x: np.sin(0.9*x).flatten()
+# x2 = lambda x: 2*np.ones_like(x)
 
 # Sample some input points and noisy versions of the function evaluated at
 # these points.
 N = 10		# number of training points
 n = 500		# number of test points
-T = np.random.uniform(-5, 5, size=(N,))
-x = x1(T) + OBS_NOISE*np.random.randn(N)
-y = x2(T) + OBS_NOISE*np.random.randn(N)
+s = 0.00005	# noise variance
+T = np.random.uniform(-5, 0, size=(N,))
+T[-1] = 4.8 # set a goal point
+x = x1(T) + s*np.random.randn(N)
+y = x2(T) + s*np.random.randn(N)
 z = np.column_stack((T, x, y))
 
 # points we're going to make predictions at.
 Ttest = np.linspace(-5, 5, n)
 
 # Build our Gaussian process.
-# kernel = cov.summed_kernel(cov.sq_exp_kernel(1, 1), cov.noise_kernel(OBS_NOISE))
-kernel = cov.summed_kernel(cov.sq_exp_kernel(5, 1), cov.noise_kernel(0))
-kernel = cov.sq_exp_kernel(5, 1)
+# kernel = cov.sq_exp_kernel(3.2, 1)
+kernel = cov.summed_kernel(cov.sq_exp_kernel(3.2, 1), cov.noise_kernel(s))
+# kernel = cov.summed_kernel(cov.sq_exp_kernel(3.162, 1), cov.noise_kernel(0.31))
 gp = GaussianProcess(z, Ttest, kernel)
 
 # PLOTS:
@@ -50,7 +52,7 @@ pl.axis([-5, 5, -3, 3])
 
 # illustrate the possible paths.
 pl.figure(3)
-pl.plot(x, y, 'r+', ms=20)
+pl.plot(x, y, 'bx', ms=20)
 pl.plot(x1(Ttest), x2(Ttest), 'b-')
 # pl.gca().fill_between(Ttest.flat, mu-3*s, mu+3*s, color="#dddddd") # how to draw this?
 pl.plot(gp.xmu, gp.ymu, 'r--', lw=2)
