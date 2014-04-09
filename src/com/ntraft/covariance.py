Temporarily disable all covariance function special-cases except for the noise kernel's. The paths look pretty nice now but there's still no indication of whether they're correct.

src/com/ntraft/covariance.py
@@ -26,7 +26,7 @@ def sq_exp_iso(a, b, kind, l, sigma2=1):
 		return sigma2 * np.exp(-.5 * sq_dist(a/l, b/l))
 
 def matern(a, b, kind, l, sigma2=1):
-	if kind == 'test':
+	if kind == 'test' and False:
 		return sigma2
 	else:
 		a = np.sqrt(5) * a/l;
@@ -35,8 +35,8 @@ def matern(a, b, kind, l, sigma2=1):
 		return sigma2 * np.exp(-np.sqrt(r)) * (1 + np.sqrt(r) + r/3);
 
 def linear(a, b, kind, sigma2=1):
-	if kind == 'test':
-		r = np.sum(a*b)
+	if kind == 'test' and False:
+		r = np.sum((a*b).reshape(-1,1), 1).reshape(-1,1)
 	else:
 		r = np.outer(a, b)
 	return (1 + r) * sigma2

src/com/ntraft/gp.py
@@ -30,7 +30,7 @@ class GaussianProcess:
 		Kss = kernel(testpoints, testpoints, 'test')
 		Kss += 1e-9*np.eye(Kss.shape[0])
 		self.prior_L = np.linalg.cholesky(Kss)
-		self.L = np.linalg.cholesky(Kss - np.dot(v.T, v)) # TODO not positive definite?
+		self.L = np.linalg.cholesky(Kss - np.dot(v.T, v))
 	
 	def sample(self, n=1):
 		'''

src/com/ntraft/gpdemo.py
@@ -21,14 +21,14 @@ N = 20		# number of training points
 n = 30		# number of test points
 s = 0.00000	# noise variance
 # T = np.random.uniform(-5, 0, size=(N,))
-T = np.linspace(-20, 0, N)
-T[-1] = 39 # set a goal point
+T = np.linspace(-10, 0, N)
+T[-1] = 19.6 # set a goal point
 x = x1(T) + s*np.random.randn(N)
 y = x2(T) + s*np.random.randn(N)
 z = np.column_stack((T, x, y))
 
 # points we're going to make predictions at.
-Ttest = np.linspace(0, 40, n)
+Ttest = np.linspace(0, 20, n)
 
 # Build our Gaussian process.
 # kernel = cov.sq_exp_kernel(3.2, 1)
@@ -66,7 +66,7 @@ pl.subplot(1,2,1)
 pl.plot(x, y, 'yo', ms=8)
 pl.plot(xs, ys)
 pl.title('Five samples from the GP posterior')
-pl.axis([-30, 70, -30, 70])
+pl.axis([-10, 25, -10, 25])
 
 # illustrate the possible paths.
 pl.subplot(1,2,2)
@@ -79,6 +79,6 @@ pl.title('Mean of {} samples'.format(ns))
 # pl.plot(x1(Ttest), x2(Ttest), 'b-')
 # pl.plot(xgp.mu, ygp.mu, 'r--', lw=2)
 # pl.title('Mean predictions')
-pl.axis([-30, 70, -30, 70])
+pl.axis([-10, 25, -10, 25])
 
 pl.show()